61337f6579aca7949766459273d32241dcd0f237
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-01-11 17:30:03 +0700

    Fix Taking Photo and Display it

--- app/src/main/java/com/example/tngp17_001/mymoregistration/ui/VerifyCustomerInfoActivity.java
+++ app/src/main/java/com/example/tngp17_001/mymoregistration/ui/VerifyCustomerInfoActivity.java
@@ -6,16 +6,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -23,15 +20,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.example.tngp17_001.mymoregistration.BuildConfig;
 import com.example.tngp17_001.mymoregistration.R;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -39,16 +31,15 @@ public class VerifyCustomerInfoActivity extends AppCompatActivity {
 
     ImageButton backBtn;
     Button cameraBtn;
-    private static final int MY_CAMERA_REQUEST_CODE = 100;
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
+    Button nextBtn;
+    private static final int MY_CAMERA_REQUEST_CODE = 0;
+    public static final int REQUEST_CAMERA = 2;
+    ImageView imageView;
+    Uri uri;
 
-    private Uri mImageUri;
-    private String pictureImagePath = "";
-    String mCurrentPhotoPath;
-    private static final int REQUEST_TAKE_PHOTO = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        permissionCheck();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_customer_info);
         View decorView = getWindow().getDecorView();
@@ -64,15 +55,18 @@ public class VerifyCustomerInfoActivity extends AppCompatActivity {
             if (actionBar != null) actionBar.hide();
         }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        if (checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_CAMERA_REQUEST_CODE);
-        }
 
-        this.imageView = this.findViewById(R.id.imageView1);
-
+        imageView = findViewById(R.id.imageView1);
         backBtn = findViewById(R.id.btn_back);
+        nextBtn = findViewById(R.id.btn_next);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(VerifyCustomerInfoActivity.this, TermsAndConditionsActivity.class);
+                startActivity(i);
+            }
+        });
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,95 +76,44 @@ public class VerifyCustomerInfoActivity extends AppCompatActivity {
         cameraBtn = findViewById(R.id.btn_camera);
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                try {
-                    dispatchTakePictureIntent();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String timeStamp =
+                        new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "IMG_" + timeStamp + ".jpg";
+                File f = new File(Environment.getExternalStorageDirectory()
+                        , "DCIM/Camera/" + imageFileName);
+                uri = Uri.fromFile(f);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(Intent.createChooser(intent
+                        , "Take a picture with"), REQUEST_CAMERA);
             }
         });
     }
 
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), "Camera");
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-    private void dispatchTakePictureIntent() throws IOException {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            getContentResolver().notifyChange(uri, null);
+            ContentResolver cr = getContentResolver();
             try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                return;
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-               // Uri photoURI = Uri.fromFile(createImageFile());
-
-                Uri photoURI = FileProvider.getUriForFile(VerifyCustomerInfoActivity.this,
-                        BuildConfig.APPLICATION_ID + ".provider",
-                        createImageFile());
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(getApplicationContext()
+                        , uri.getPath(), Toast.LENGTH_SHORT).show();
+                nextBtn.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
-    private File createTemporaryFile(String part, String ext) throws Exception
-    {
-        File tempDir= Environment.getExternalStorageDirectory();
 
-        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
-
-        if(!tempDir.exists())
-        {
-            tempDir.mkdirs();
-        }
-        return File.createTempFile(part, ext, tempDir);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            // Show the thumbnail on ImageView
-            Uri imageUri = Uri.parse(mCurrentPhotoPath);
-            File file = new File(imageUri.getPath());
-            try {
-                InputStream ims = new FileInputStream(file);
-                imageView.setImageBitmap(BitmapFactory.decodeStream(ims));
-            } catch (FileNotFoundException e) {
-                return;
-            }
-
-            // ScanFile so it will be appeared on Gallery
-            MediaScannerConnection.scanFile(VerifyCustomerInfoActivity.this,
-                    new String[]{imageUri.getPath()}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                        }
-                    });
+    public void permissionCheck() {
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_CAMERA_REQUEST_CODE);
         }
     }
-
-
 
     @Override
 
