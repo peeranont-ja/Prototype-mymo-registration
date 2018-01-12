ab46b07715476c880bd7cecd27b635cbbea318de
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-01-12 12:06:00 +0700

    Add Summary Page and Add Material Dialog Dependencies

--- app/src/main/java/com/example/tngp17_001/mymoregistration/ui/SummaryActivity.java
+++ app/src/main/java/com/example/tngp17_001/mymoregistration/ui/SummaryActivity.java
@@ -1,20 +1,32 @@
 package com.example.tngp17_001.mymoregistration.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.tngp17_001.mymoregistration.R;
 
 public class SummaryActivity extends AppCompatActivity {
 
     ImageButton backBtn;
+    Button finishBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final MaterialDialog.Builder builder = new MaterialDialog.Builder(this)
+                .title("ต้องการทำรายการต่อ?")
+                .content("กรุณากรอกรหัสผ่านเพื่อใช้งานต่อ")
+                .positiveText("เข้าสู่ระบบ")
+                .negativeText("ยกเลิก")
+                .cancelable(false)
+                .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
@@ -33,11 +45,26 @@ public class SummaryActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         backBtn = findViewById(R.id.btn_back);
+        finishBtn = findViewById(R.id.btn_finish);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                MaterialDialog dialog = builder.build();
+//                dialog.show();
+                MaterialDialog dialog = new MaterialDialog.Builder(SummaryActivity.this)
+                        .title("Title")
+                        .content("content")
+                        .positiveText("agree")
+                        .show();
+            }
+        });
+
+
     }
 }
