84fcd312536cfa191972e38ae3035f0b140ae277
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-01-15 10:27:18 +0700

    Add verifyPassword/ sendSMS/ success dialog

--- app/src/main/java/com/example/tngp17_001/mymoregistration/ui/SummaryActivity.java
+++ app/src/main/java/com/example/tngp17_001/mymoregistration/ui/SummaryActivity.java
@@ -1,21 +1,27 @@
 package com.example.tngp17_001.mymoregistration.ui;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.GravityEnum;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.tngp17_001.mymoregistration.R;
+
+import static com.example.tngp17_001.mymoregistration.R.layout.dialog_send_sms;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -43,51 +49,37 @@ public class SummaryActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         backBtn = findViewById(R.id.btn_back);
-        finishBtn = findViewById(R.id.btn_finish);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        final Dialog dialog = new Dialog(SummaryActivity.this);
+        dialog.setTitle("Password Dialog");
+        dialog.setContentView(R.layout.dialog_staff_confirm);
+        final Dialog dialogSMS = new Dialog(SummaryActivity.this);
+        dialogSMS.setTitle("SMS Dialog");
+        dialogSMS.setContentView(R.layout.dialog_send_sms);
+
+        Button sendPwdBtn = dialog.findViewById(R.id.btn_send_password);
+
+        finishBtn = findViewById(R.id.btn_finish);
         finishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder =
-                        new AlertDialog.Builder(SummaryActivity.this);
-                LayoutInflater inflater = getLayoutInflater();
-
-                View view = inflater.inflate(R.layout.dialog_staff_confirm, null);
-                builder.setView(view);
-
-                builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Check username password
-//                        if (username.getText().equals("demo@example.com") &&
-//                                password.getText().equals("demo")) {
-//                            Toast.makeText(getApplicationContext(), "Login success!",
-//                                    Toast.LENGTH_SHORT).show();
-//                        } else {
-//                            Toast.makeText(getApplicationContext(), "Login Failed!",
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-                    }
-                });
-//                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//
-//                    }
-//                });
-
-                builder.create();
-
-                builder.show();
-
+                dialog.show();
             }
         });
 
+        sendPwdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                dialogSMS.show();
+            }
+        });
 
     }
 }
