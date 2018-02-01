48a3d73c3b5afa20dc5ea464f39e1546d55eb08e
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-02-01 12:02:38 +0700

    Add Barcode Scanner

--- mymoregistration2ndedition/src/main/java/com/example/mymoregistration2ndedition/BindBookActivity.java
+++ mymoregistration2ndedition/src/main/java/com/example/mymoregistration2ndedition/BindBookActivity.java
@@ -1,5 +1,6 @@
 package com.example.mymoregistration2ndedition;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,16 +8,28 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class BindBookActivity extends AppCompatActivity {
 
     Button nextBtn;
+    TextView bookBankResult;
+    ImageButton searchBtn;
     ImageButton backBtn;
+
+    int REQUEST_CODE = 191;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bind_book);
+
+        bookBankResult = findViewById(R.id.book_bank_result);
+        nextBtn = findViewById(R.id.btn_next);
+        bookBankResult.setText("");
+        nextBtn.setVisibility(View.INVISIBLE);
+
 
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -37,14 +50,31 @@ public class BindBookActivity extends AppCompatActivity {
             }
         });
 
-        nextBtn = findViewById(R.id.btn_next);
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent i = new Intent(BindBookActivity.this, DepositOpenAccountActivity.class);
-                Intent i = new Intent(BindBookActivity.this, ScanBarcodeActivity.class);
+                Intent i = new Intent(BindBookActivity.this, DepositOpenAccountActivity.class);
                 startActivity(i);
             }
         });
+
+        searchBtn = findViewById(R.id.btn_search);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(BindBookActivity.this, ScanBarcodeActivity.class);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 191) {
+            if (resultCode == Activity.RESULT_OK) {
+                String barcodeNumber = data.getStringExtra("barcodeNumber");
+                bookBankResult.setText(barcodeNumber);
+                nextBtn.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- mymoregistration2ndedition/src/main/java/com/example/mymoregistration2ndedition/ScanBarcodeActivity.java
+++ mymoregistration2ndedition/src/main/java/com/example/mymoregistration2ndedition/ScanBarcodeActivity.java
@@ -1,8 +1,11 @@
 package com.example.mymoregistration2ndedition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
@@ -14,6 +17,18 @@ public class ScanBarcodeActivity extends AppCompatActivity implements ZBarScanne
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+        else {
+            if (actionBar != null) actionBar.hide();
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         mScannerView = new ZBarScannerView(this);
         setContentView(mScannerView);
     }
@@ -34,8 +49,15 @@ public class ScanBarcodeActivity extends AppCompatActivity implements ZBarScanne
     @Override
     public void handleResult(Result rawResult) {
         // Do something with the result here
-        Log.d("kuy", rawResult.getContents()); // Prints scan results
-        Log.d("kuy", rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+        Log.d("scan result", rawResult.getContents()); // Prints scan results
+        Log.d("scan result", rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+//        Intent i = new Intent(ScanBarcodeActivity.this, BindBookActivity.class);
+        Intent i = new Intent();
+        i.putExtra("barcodeNumber", rawResult.getContents());
+        setResult(RESULT_OK, i);
+        finish();
+//        startActivity(i);
 
         // If you would like to resume scanning, call this method below:
         mScannerView.resumeCameraPreview(this);
